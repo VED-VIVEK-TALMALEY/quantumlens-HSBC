a2095b6698f6de4aeca7b99e136ea1e82e2586d9
VED-VIVEK-TALMALEY <vedwork21@gmail.com>
2026-07-10 09:14:32 +0530

    vercel fix patched

--- src/api/main.py
+++ src/api/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-            "https://your-vercel-app.vercel.app",
+            "https://quantumlens-hsbc.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
