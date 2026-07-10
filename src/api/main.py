fe8eb95090de944c2d67be20760705188410a36e
VED-VIVEK-TALMALEY <vedwork21@gmail.com>
2026-07-10 08:05:01 +0530

    quantlumens version 1 render bug solved

--- src/api/main.py
+++ src/api/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+            "https://your-vercel-app.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
