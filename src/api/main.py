42b7ddf5fd0028d11a8fd412764a6d759038fd83
VED-VIVEK-TALMALEY <vedwork21@gmail.com>
2026-07-10 20:15:25 +0530

    cors error patch fix try1

--- src/api/main.py
+++ src/api/main.py
@@ -16,8 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-            "https://quantumlens-hsbc.vercel.app/",
-            
+        "https://quantumlens-hsbc.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
